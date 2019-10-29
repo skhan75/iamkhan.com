ba35d22f28d40e7b8ddddf79a89e3b2459c57fc0
skhan75 <sami.ahmadkhan12@gmail.com>
2019-10-29 14:08:43 -0700

    Adding Nav bar elements base

---
+++ src/components/Header/nav.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import data from '../../data/contact';
+import '../../styling/layout/sidebar.css';
+
+const Nav = () => {
+  return (
+    <section id="sidebar">
+
+      <section id="intro">
+        <Link to="/" className="logo">
+          LOGO
+        </Link>
+        <header>
+          <h2>Sami Ahmad Khan</h2>
+          <p><a href="mailto:sami.ahmadkhan12@gmail.com">sami.ahmadkhan12@gmail.com</a></p>
+        </header>
+      </section>
+
+      <section className="blurb">
+
+        <h2>About</h2>
+        <p>
+          Hi I&apos;m Sami. I like building things.
+        </p>
+
+        <ul className="actions">
+          <li>
+            {
+              window.location.pathname !== `/resume`
+              ?
+              <Link to="/resume" className="button">Learn More</Link>
+              :
+              <Link to="/about" className="button">About Me</Link>
+            }
+          </li>
+        </ul>
+
+      </section>
+
+      <section id="footer">
+        <ul className="icons">
+          { data.map((s) => (
+            <li key={s.label}>
+              <a href={s.link}>
+                <FontAwesomeIcon icon={s.icon} />
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <p className="copyright">&copy; Sami Ahmad Khan <Link to="/">iamkhan.com</Link>.</p>
+      </section>
+
+    </section>
+  )
+}
+
+export default Nav;
--- src/hoc/layouts/main.js
+++ src/hoc/layouts/main.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 
 
 import Header from '../../components/Header/header';
+import Nav from '../../components/Header/nav';
 
 
 class Main extends Component {
   render() {
     return (
       <div>
+        <Nav />
         <Header />
+
       </div>
     )
   }
